55cdbf046b0509522a9e9c830eb2641a700c6943
Tommy Lettieri <tommylettieri@gmail.com>
2020-04-25 02:55:43 -0400

    Added readline logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Initial output');
+import fs = require('fs');
+
+const readFile  = (filePath:string) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, (err,data) => {
+            if(err) {
+                return reject(err);
+            } else {
+                return resolve(data.toString());
+            }
+        });
+    })
+};
+
+const defFilePath = process.argv[2];
+console.log(defFilePath);
+
+(async () => {
+    const content = await readFile(defFilePath);
+    console.log(content);
+})();
